82d314b7e1b61e8eeaf7a4f274d58a5ca51b7e4c
pooky <verret.benoit@gmail.com>
2015-11-05 21:35:27 -0500

    Added python files.

---
+++ 1080p.py
@@ -0,0 +1,11 @@
+import bpy
+
+# 1080p preset
+bpy.context.scene.render.display_mode = 'NONE'
+bpy.ops.script.python_file_run(filepath="/usr/share/blender/scripts/presets/render/HDTV_1080p.py")
+bpy.context.scene.render.antialiasing_samples = '16'
+bpy.context.scene.render.image_settings.file_format = 'H264'
+bpy.types.ImageFormatSettings.file_format = 'H264'
+bpy.ops.script.python_file_run(filepath="/usr/share/blender/scripts/presets/ffmpeg/h264.py")
+bpy.context.scene.render.ffmpeg.video_bitrate = 32000
+bpy.context.scene.render.ffmpeg.audio_codec = 'MP3'
