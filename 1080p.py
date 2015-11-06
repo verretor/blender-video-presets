594dfd33107f5a3caa5d98f289df532daad3c829
pooky <verret.benoit@gmail.com>
2015-11-05 22:06:11 -0500

    Make scripts platform independent.
    Don't use scripts in /usr/share/...

--- 1080p.py
+++ 1080p.py
@@ -2,16 +2,37 @@ import bpy
 
 # 1080p preset
 bpy.context.scene.render.display_mode = 'NONE'
-bpy.ops.script.python_file_run(filepath="/usr/share/blender/scripts/presets/render/HDTV_1080p.py")
+bpy.context.scene.render.resolution_x = 1920
+bpy.context.scene.render.resolution_y = 1080
+bpy.context.scene.render.resolution_percentage = 100
+bpy.context.scene.render.pixel_aspect_x = 1
+bpy.context.scene.render.pixel_aspect_y = 1
 
 # Adjust frame rate accordingly
 # 24 fps with base 1.001 = 23.98 fps
 bpy.context.scene.render.fps = 24
 bpy.context.scene.render.fps_base = 1.001
+#bpy.context.scene.render.fps_base = 1
 
 bpy.context.scene.render.antialiasing_samples = '16'
 bpy.context.scene.render.image_settings.file_format = 'H264'
 bpy.types.ImageFormatSettings.file_format = 'H264'
-bpy.ops.script.python_file_run(filepath="/usr/share/blender/scripts/presets/ffmpeg/h264.py")
+
+is_ntsc = (bpy.context.scene.render.fps != 25)
+
+bpy.context.scene.render.ffmpeg.format = "H264"
+bpy.context.scene.render.ffmpeg.codec = "H264"
+
+if is_ntsc:
+    bpy.context.scene.render.ffmpeg.gopsize = 18
+else:
+    bpy.context.scene.render.ffmpeg.gopsize = 15
+
 bpy.context.scene.render.ffmpeg.video_bitrate = 32000
+bpy.context.scene.render.ffmpeg.maxrate = 9000
+bpy.context.scene.render.ffmpeg.minrate = 0
+bpy.context.scene.render.ffmpeg.buffersize = 224 * 8
+bpy.context.scene.render.ffmpeg.packetsize = 2048
+bpy.context.scene.render.ffmpeg.muxrate = 10080000
+
 bpy.context.scene.render.ffmpeg.audio_codec = 'MP3'
--- 720p.py
+++ 720p.py
@@ -2,8 +2,11 @@ import bpy
 
 # 720p preset
 bpy.context.scene.render.display_mode = 'NONE'
-bpy.ops.script.python_file_run(filepath="/usr/share/blender/scripts/presets/render/HDTV_720p.py")
-
+bpy.context.scene.render.resolution_x = 1280
+bpy.context.scene.render.resolution_y = 720
+bpy.context.scene.render.resolution_percentage = 100
+bpy.context.scene.render.pixel_aspect_x = 1
+bpy.context.scene.render.pixel_aspect_y = 1
 # Adjust frame rate accordingly
 # fps 30 with base 1.001 = 29.97 fps.
 bpy.context.scene.render.fps = 30
@@ -12,6 +15,22 @@ bpy.context.scene.render.fps_base = 1.000
 bpy.context.scene.render.antialiasing_samples = '16'
 bpy.context.scene.render.image_settings.file_format = 'H264'
 bpy.types.ImageFormatSettings.file_format = 'H264'
-bpy.ops.script.python_file_run(filepath="/usr/share/blender/scripts/presets/ffmpeg/h264.py")
+
+is_ntsc = (bpy.context.scene.render.fps != 25)
+
+bpy.context.scene.render.ffmpeg.format = "H264"
+bpy.context.scene.render.ffmpeg.codec = "H264"
+
+if is_ntsc:
+    bpy.context.scene.render.ffmpeg.gopsize = 18
+else:
+    bpy.context.scene.render.ffmpeg.gopsize = 15
+
 bpy.context.scene.render.ffmpeg.video_bitrate = 32000
+bpy.context.scene.render.ffmpeg.maxrate = 9000
+bpy.context.scene.render.ffmpeg.minrate = 0
+bpy.context.scene.render.ffmpeg.buffersize = 224 * 8
+bpy.context.scene.render.ffmpeg.packetsize = 2048
+bpy.context.scene.render.ffmpeg.muxrate = 10080000
+
 bpy.context.scene.render.ffmpeg.audio_codec = 'MP3'
