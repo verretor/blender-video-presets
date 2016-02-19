1cd14a0d984ddeceeb3a1e76a4624cbea68c677d
pooky <verret.benoit@gmail.com>
2016-02-19 06:19:44 -0500

    Faster video previews for 720p.

--- 720p.py
+++ 720p.py
@@ -1,7 +1,7 @@
 import bpy
 
 # 720p preset
-bpy.context.scene.render.display_mode = 'NONE'
+bpy.context.scene.render.display_mode = 'WINDOW'
 bpy.context.scene.render.resolution_x = 1280
 bpy.context.scene.render.resolution_y = 720
 bpy.context.scene.render.resolution_percentage = 100
@@ -28,3 +28,6 @@ bpy.context.scene.render.ffmpeg.packetsize = 2048
 bpy.context.scene.render.ffmpeg.muxrate = 10080000
 
 bpy.context.scene.render.ffmpeg.audio_codec = 'MP3'
+
+# Faster previews
+bpy.data.screens['Video Editing'].areas[4].spaces[0].proxy_render_size = 'PROXY_25'
